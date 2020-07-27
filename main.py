db29e43a7a3521f7df21c4fab9f3aee7af36b52b
BenjaminKing39 <bekingly39@gmail.com>
2020-07-27 17:18:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+Score = 0
+
+isPlayer1 = True
+
+def Function1(i):
+ if i ==1:
+  return True
+
+def Function2(i):
+  return(i+1)
+
+running = True
+
+while running:
+ if isPlayer1 is True:
+    print ('Player1')
+ else:
+   print ('Player2')
+
+  if function1 = True 
+  print('Player One') 
+
+
+
+
+
+
+#def addone(i):
+  #return(i+1)
+
+#def addtwo(i):
+  #return(i+2)
+ 
+  
+ #if(str(mynamenoa(n,l)) == ss
+ #a = int(input("number: "))
+
+#print(addone(a))
+#print(addtwo(a))
+
